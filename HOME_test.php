aaefdc3af5800a6676b623b8338328f93f792824
masayoshi <2101138@s.asojuku.ac.jp>
2022-12-08 20:25:07 +0900

    a

---
+++ HOME_test.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" /></head><body><div class="v604_1111"><div class="v604_1112"><span class="v604_1113">okado</span></div><div class="name"></div><div class="v604_1115"><div class="v604_1116"><span class="v604_1117">image</span></div><div class="v604_1118"><span class="v604_1119">ネックレス</span></div><div class="v604_1120"><span class="v604_1121">イヤリング</span></div><div class="v604_1122"><span class="v604_1123">ピアス</span></div><div class="v604_1124"><span class="v604_1125">リング</span></div><div class="v604_1126"><span class="v604_1127">アイテム</span></div><div class="v604_1128"><span class="v604_1129">pick up</span></div></div></div></body></html> <br/><br/> <style>* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v604_1111 {
+  width: 360px;
+  height: 800px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v604_1112 {
+  width: 360px;
+  height: 200px;
+  background: rgba(255,255,255,0.8399999737739563);
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 40px;
+  left: 0px;
+  overflow: hidden;
+}
+.v604_1113 {
+  width: 340px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 48px;
+  opacity: 1;
+  text-align: center;
+}
+.name {
+  color: #fff;
+}
+.v604_1115 {
+  width: 360px;
+  height: 560px;
+  background: rgba(255,255,255,1);
+  margin: 35px;
+  opacity: 1;
+  position: absolute;
+  top: 240px;
+  left: 0px;
+  overflow: hidden;
+}
+.v604_1116 {
+  width: 200px;
+  height: 130px;
+  background: rgba(217,217,217,1);
+  padding: 48px 50px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 30px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1117 {
+  width: 100px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 48px;
+  left: 50px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1118 {
+  width: 200px;
+  height: 44px;
+  background: url("../images/v604_1118.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 30px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 201px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1119 {
+  width: 140px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 30px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1120 {
+  width: 200px;
+  height: 40px;
+  background: url("../images/v604_1120.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 30px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 274px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1121 {
+  width: 140px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 30px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1122 {
+  width: 200px;
+  height: 40px;
+  background: url("../images/v604_1122.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 30px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 343px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1123 {
+  width: 140px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 30px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1124 {
+  width: 200px;
+  height: 40px;
+  background: url("../images/v604_1124.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 30px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 412px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1125 {
+  width: 140px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 30px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1126 {
+  width: 200px;
+  height: 40px;
+  background: url("../images/v604_1126.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 30px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 481px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1127 {
+  width: 140px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 30px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v604_1128 {
+  width: 80px;
+  height: 30px;
+  background: url("../images/v604_1128.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 80px;
+  overflow: hidden;
+}
+.v604_1129 {
+  width: 60px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: center;
+}
+</style>
---
+++ syouhinitirann_test.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" /></head><body><div class="v534_1133"><div class="v534_1134"><span class="v534_1135">ネックレス</span></div><div class="name"></div><div class="name"></div><div class="name"></div><div class="name"></div><div class="name"></div><div class="name"></div><div class="name"></div></div></body></html> <br/><br/> <style>* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v534_1133 {
+  width: 360px;
+  height: 1120px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v534_1134 {
+  width: 360px;
+  height: 120px;
+  background: url("../images/v534_1134.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v534_1135 {
+  width: 340px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 32px;
+  opacity: 1;
+  text-align: center;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+</style>
